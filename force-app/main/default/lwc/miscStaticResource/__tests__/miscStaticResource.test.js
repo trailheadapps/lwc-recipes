d8e7c0ccb812f2cc90fd71b1b33fefe19c1471df
muenzpraeger <rwinkelmeyer@salesforce.com>
2019-01-13 20:34:02 -0700

    Adding clarification comments for resourceUrl

--- force-app/main/default/lwc/miscStaticResource/__tests__/miscStaticResource.test.js
+++ force-app/main/default/lwc/miscStaticResource/__tests__/miscStaticResource.test.js
@@ -14,6 +14,7 @@ describe('c-misc-static-resource', () => {
             'img[alt="Trailhead logo"]',
         );
         expect(imgTrailheadEl).not.toBeNull();
+        // lwc-jest automocks @salesforce/resourceUsl, and returns localhost/name_of_resource.
         expect(imgTrailheadEl.src).toBe('http://localhost/trailhead_logo');
 
         // Query for img element that uses a static image resource within a zip file
@@ -21,6 +22,7 @@ describe('c-misc-static-resource', () => {
             'img[alt="Einstein logo"]',
         );
         expect(imgEinsteinEl).not.toBeNull();
+        // lwc-jest automocks @salesforce/resourceUsl, and returns localhost/name_of_resource.
         expect(imgEinsteinEl.src).toBe(
             'http://localhost/trailhead_characters/images/einstein.png',
         );
