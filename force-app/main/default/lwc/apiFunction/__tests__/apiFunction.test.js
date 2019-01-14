cc634204017485139a57c397724edc1100a1f569
muenzpraeger <rwinkelmeyer@salesforce.com>
2019-01-14 16:54:11 +0100

    Remove not needed element test

--- force-app/main/default/lwc/apiFunction/__tests__/apiFunction.test.js
+++ force-app/main/default/lwc/apiFunction/__tests__/apiFunction.test.js
@@ -2,32 +2,6 @@ import { createElement } from 'lwc';
 import ApiFunction from 'c/apiFunction';
 
 describe('c-api-function', () => {
-    afterEach(() => {
-        // The jsdom instance is shared across test cases in a single file so reset the DOM
-        while (document.body.firstChild) {
-            document.body.removeChild(document.body.firstChild);
-        }
-    });
-
-    it('renders a lightning-button and a c-clock element', () => {
-        // Create initial element
-        const element = createElement('c-api-function', {
-            is: ApiFunction,
-        });
-        document.body.appendChild(element);
-
-        // Query lightning-button element
-        const lightningButtonEl = element.shadowRoot.querySelector(
-            'lightning-button',
-        );
-        expect(lightningButtonEl).not.toBeNull();
-        expect(lightningButtonEl.label).toBe('Refresh Time');
-
-        // Query lightning-button component element
-        const clockEl = element.shadowRoot.querySelector('c-clock');
-        expect(clockEl).not.toBeNull();
-    });
-
     it('calls the public function "refresh" on the c-clock component', () => {
         // Create initial element
         const element = createElement('c-api-function', {
