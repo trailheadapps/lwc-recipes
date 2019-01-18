da9e8c24c9f032a1d882708043ef8fe2e62a471f
René Winkelmeyer <muenzpraeger@users.noreply.github.com>
2019-01-18 08:44:45 +0100

    Jest tests for composition (#33)
    * First addition of composition tests
    
    * Formatting updates
    
    * Cc/isexposed (#30)
    
    * adding path warning to readme
    
    * readme edit
    
    * readme edit
    
    * adjusting isExposed in meta file
    
    * Composition test update
    
    * Updating Apex test
    
    * Updating Apex promise test

---
+++ force-app/main/default/lwc/compositionBasics/__tests__/compositionBasics.test.js
@@ -0,0 +1,40 @@
+import { createElement } from 'lwc';
+import CompositionBasics from 'c/compositionBasics';
+
+describe('c-composition-basics', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('renders one contact tile', () => {
+        // Create initial element
+        const element = createElement('c-composition-basics', {
+            is: CompositionBasics
+        });
+        document.body.appendChild(element);
+
+        // Select rendered contact tile for length check
+        const contactTileEls = element.shadowRoot.querySelectorAll(
+            'c-contact-tile'
+        );
+        expect(contactTileEls.length).toBe(1);
+    });
+
+    it('renders with contact tile properties set', () => {
+        // Create initial element
+        const element = createElement('c-composition-basics', {
+            is: CompositionBasics
+        });
+        document.body.appendChild(element);
+
+        // Select contact tile for public property check
+        const contactTileEl = element.shadowRoot.querySelector(
+            'c-contact-tile'
+        );
+        expect(contactTileEl.contact.Name).toBe('Amy Taylor');
+        expect(contactTileEl.contact.Title).toBe('VP of Engineering');
+    });
+});
---
+++ force-app/main/default/lwc/compositionWithAppBuilder/__tests__/compositionWithAppBuilder.test.js
@@ -0,0 +1,26 @@
+import { createElement } from 'lwc';
+import CompositionWithAppBuilder from 'c/compositionWithAppBuilder';
+
+describe('c-composition-with-app-builder', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('reflects public property values', () => {
+        // Create initial element
+        const element = createElement('c-composition-with-app-builder', {
+            is: CompositionWithAppBuilder
+        });
+        element.picklistValue = 'somePicklist';
+        element.stringValue = 'someString';
+        element.numberValue = 99;
+        document.body.appendChild(element);
+
+        expect(element.picklistValue).toBe('somePicklist');
+        expect(element.stringValue).toBe('someString');
+        expect(element.numberValue).toBe(99);
+    });
+});
