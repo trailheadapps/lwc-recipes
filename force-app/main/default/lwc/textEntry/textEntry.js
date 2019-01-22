929115f534862f0266b557798f091faa0e3f2f32
Pat McClellan <pat@proton7group.com>
2019-01-21 16:25:51 -0800

    update

--- force-app/main/default/lwc/textEntry/textEntry.js
+++ force-app/main/default/lwc/textEntry/textEntry.js
@@ -1,12 +1,10 @@
 import { LightningElement, track } from 'lwc';
 
 export default class TextEntry extends LightningElement {
-    @track body;
     @track charsLeft = 160;
 
     inputChange(event){
-        this.body = event.target.name;
-        this.charsLeft = 160 - this.body.length;
-
+        const bodyInput = event.target.name;
+        this.charsLeft = 160 - bodyInput.length;
     }
 }
