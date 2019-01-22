4b320ed3c4c6a711d747ca211244c4031d1d63d2
Pat McClellan <pat@proton7group.com>
2019-01-21 16:52:33 -0800

    test

--- force-app/main/default/lwc/textEntry/textEntry.js
+++ force-app/main/default/lwc/textEntry/textEntry.js
@@ -4,8 +4,8 @@ export default class TextEntry extends LightningElement {
     @track textInput;
     @track charsLeft = 160;
 
-    inputChange(){
-        // const bodyInput = event.target.value;
+    inputChange(event){
+        this.textInput = event.target.value;
         this.charsLeft = 160 - this.textInput.length;
     }
     clearInput(){
