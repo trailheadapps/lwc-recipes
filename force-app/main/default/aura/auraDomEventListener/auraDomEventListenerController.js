40aa910f49584006896979efd60bd8fe57deb3ee
Christophe Coenraets <ccoenraets@gmail.com>
2019-01-31 15:44:03 -0500

    Changing embedded LWC recipe to use same use case in code vs app builder composition

--- force-app/main/default/aura/auraDomEventListener/auraDomEventListenerController.js
+++
@@ -1,6 +0,0 @@
-({
-    handleFilterChange: function(component, event) {
-        var filters = event.getParam('filters');
-        component.set('v.message', filters.length > 0 ? 'Your selection: ' + filters.join() : 'No selection');
-    },
-});
