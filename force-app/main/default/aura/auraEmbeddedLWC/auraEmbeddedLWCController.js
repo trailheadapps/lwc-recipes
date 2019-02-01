ac2e5768461cd0a9c66351bb77ba7384200755fb
Christophe Coenraets <ccoenraets@gmail.com>
2019-02-01 15:58:50 -0500

    Changing embedded LWC recipe to use same use case in code vs app builder composition (#50)

---
+++ force-app/main/default/aura/auraEmbeddedLWC/auraEmbeddedLWCController.js
@@ -0,0 +1,7 @@
+({
+    handleContactSelect: function(component, event) {
+        component.set('v.contactId', event.getParam('contactId'));
+        var service = component.find('service');
+        service.reloadRecord();
+    },
+});
---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,17 @@
+import { LightningElement, wire } from 'lwc';
+import getContactList from '@salesforce/apex/ContactController.getContactList';
+
+export default class EventBubbling extends LightningElement {
+    @wire(getContactList) contacts;
+
+    handleSelect(event) {
+        // 1. Prevent default behavior of anchor tag click which is to navigate to the href url
+        event.preventDefault();
+        // 2. Create a custom event that bubbles. Read about event best practices at http://developer.salesforce.com/docs/component-library/documentation/lwc/lwc.events_best_practices
+        const selectEvent = new CustomEvent('contactselect', {
+            detail: { contactId: event.currentTarget.dataset.contactId }
+        });
+        // 3. Fire the custom event
+        this.dispatchEvent(selectEvent);
+    }
+}
