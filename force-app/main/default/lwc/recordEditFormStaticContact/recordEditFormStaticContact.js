6cb05babf81ae8c104ffc01d9c49ca736bcc611e
Christophe Coenraets <ccoenraets@gmail.com>
2019-02-01 15:58:35 -0500

    Adding Account field to record*form recipes to show different UI elems based on metadata (#49)

--- force-app/main/default/lwc/recordEditFormStaticContact/recordEditFormStaticContact.js
+++ force-app/main/default/lwc/recordEditFormStaticContact/recordEditFormStaticContact.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.AccountId';
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import TITLE_FIELD from '@salesforce/schema/Contact.Title';
 import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
@@ -7,6 +8,7 @@ import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 
 export default class RecordEditFormStaticContact extends LightningElement {
     // Exposing fields to make them available in the template
+    accountField = ACCOUNT_FIELD;
     nameField = NAME_FIELD;
     titleField = TITLE_FIELD;
     phoneField = PHONE_FIELD;
--- force-app/main/default/lwc/recordFormStaticContact/recordFormStaticContact.js
+++ force-app/main/default/lwc/recordFormStaticContact/recordFormStaticContact.js
@@ -1,5 +1,6 @@
 import { LightningElement, api } from 'lwc';
 
+import ACCOUNT_FIELD from '@salesforce/schema/Contact.AccountId';
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 import TITLE_FIELD from '@salesforce/schema/Contact.Title';
 import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
@@ -10,5 +11,5 @@ export default class RecordFormStaticContact extends LightningElement {
     @api recordId;
     @api objectApiName;
 
-    fields = [NAME_FIELD, TITLE_FIELD, PHONE_FIELD, EMAIL_FIELD];
+    fields = [ACCOUNT_FIELD, NAME_FIELD, TITLE_FIELD, PHONE_FIELD, EMAIL_FIELD];
 }
