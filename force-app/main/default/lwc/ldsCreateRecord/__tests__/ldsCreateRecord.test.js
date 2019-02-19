bd88a129e149b9adc919a42c185bb5f70b8fbcdc
muenzpraeger <mail@winkelmeyer.com>
2019-02-19 09:20:21 +0100

    Add ldsCreateRecord test

---
+++ force-app/main/default/lwc/ldsCreateRecord/__tests__/ldsCreateRecord.test.js
@@ -0,0 +1,138 @@
+import { createElement } from 'lwc';
+import LdsCreateRecord from 'c/ldsCreateRecord';
+import { createRecord } from 'lightning/uiRecordApi';
+
+// Realistic data after a create record call
+const mockCreateRecord = require('./data/createRecord.json');
+
+describe('c-lds-create-record', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        // Prevent data saved on mocks from leaking between tests
+        jest.clearAllMocks();
+    });
+
+    // Helper function to wait until the microtask queue is empty. This is needed for promise timing when calling UI API
+    function flushPromises() {
+        // eslint-disable-next-line no-undef
+        return new Promise(resolve => setImmediate(resolve));
+    }
+
+    it('sets value from lightning-input field as parameter to createRecord call', () => {
+        const USER_INPUT = 'Gomez Inc.';
+        const INPUT_PARAMETERS = [
+            { apiName: 'Account', fields: { Name: USER_INPUT } }
+        ];
+
+        const element = createElement('c-lds-create-record', {
+            is: LdsCreateRecord
+        });
+        document.body.appendChild(element);
+
+        const inputEl = element.shadowRoot.querySelector(
+            'lightning-input[class="slds-m-bottom_x-small"]'
+        );
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return a promise to wait for any asynchronous DOM updates.
+        return Promise.resolve().then(() => {
+            expect(createRecord).toHaveBeenCalled();
+            expect(createRecord.mock.calls[0]).toEqual(INPUT_PARAMETERS);
+        });
+    });
+
+    it('displays an account id after record creation', () => {
+        const USER_INPUT = 'Gomez Inc.';
+        const RESULT = '0011700000r6zdbAAA';
+        createRecord.mockResolvedValue(mockCreateRecord);
+
+        const element = createElement('c-lds-create-record', {
+            is: LdsCreateRecord
+        });
+        document.body.appendChild(element);
+
+        const inputEl = element.shadowRoot.querySelector(
+            'lightning-input[class="slds-m-bottom_x-small"]'
+        );
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return a promise to wait for any asynchronous DOM updates.
+        return flushPromises().then(() => {
+            const displayEl = element.shadowRoot.querySelector(
+                'lightning-input:not([class="slds-m-bottom_x-small"])'
+            );
+            expect(displayEl.value).toBe(RESULT);
+        });
+    });
+
+    it('displays a success toast after record creation', () => {
+        const USER_INPUT = 'Gomez Inc.';
+        createRecord.mockResolvedValue(mockCreateRecord);
+
+        const element = createElement('c-lds-create-record', {
+            is: LdsCreateRecord
+        });
+        document.body.appendChild(element);
+
+        // Mock handler for toast event
+        const handler = jest.fn();
+        // Add event listener to catch toast event
+        element.addEventListener('lightning__showtoast', handler);
+
+        const inputEl = element.shadowRoot.querySelector(
+            'lightning-input[class="slds-m-bottom_x-small"]'
+        );
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return a promise to wait for any asynchronous DOM updates.
+        return Promise.resolve().then(() => {
+            // Check if toast event has been fired
+            expect(handler).toHaveBeenCalled();
+        });
+    });
+
+    it('displays an error toast on createRecord error', () => {
+        const USER_INPUT = 'invalid';
+        createRecord.mockResolvedValue = { id: '111' };
+
+        const element = createElement('c-lds-create-record', {
+            is: LdsCreateRecord
+        });
+        document.body.appendChild(element);
+
+        // Mock handler for toast event
+        const handler = jest.fn();
+        // Add event listener to catch toast event
+        element.addEventListener('lightning__showtoast', handler);
+
+        const inputEl = element.shadowRoot.querySelector(
+            'lightning-input[class="slds-m-bottom_x-small"]'
+        );
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return a promise to wait for any asynchronous DOM updates.
+        return Promise.resolve().then(() => {
+            // Check if toast event has been fired
+            expect(handler).toHaveBeenCalled();
+        });
+    });
+});
---
+++ force-app/test/jest-mocks/lightning/navigation.js
@@ -0,0 +1,34 @@
+/**
+ * For the original lightning/navigation mock that comes by default with
+ * @salesforce/lwc-jest, see:
+ * https://github.com/salesforce/lwc-jest/blob/master/src/lightning-mocks/navigation/navigation.js
+ */
+export const CurrentPageReference = jest.fn();
+
+let _pageReference, _replace;
+
+const Navigate = Symbol('Navigate');
+const GenerateUrl = Symbol('GenerateUrl');
+export const NavigationMixin = Base => {
+    return class extends Base {
+        [Navigate](pageReference, replace) {
+            _pageReference = pageReference;
+            _replace = replace;
+        }
+        [GenerateUrl](pageReference) {}
+    };
+};
+NavigationMixin.Navigate = Navigate;
+NavigationMixin.GenerateUrl = GenerateUrl;
+
+/*
+ * Tests do not have access to the internals of this mixin used by the
+ * component under test so save a reference to the arguments the Navigate method is
+ * invoked with and provide access with this function.
+ */
+export const getNavigateCalledWith = () => {
+    return {
+        pageReference: _pageReference,
+        replace: _replace
+    };
+};
