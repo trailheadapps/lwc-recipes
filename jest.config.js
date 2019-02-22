d51a8c7b5b027f50ebbf6158483519061b725048
muenzpraeger <mail@winkelmeyer.com>
2019-02-22 05:41:38 +0100

    Remove not needed mock

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,4 @@
 const { jestConfig } = require('@salesforce/lwc-jest/config');
 module.exports = {
-    ...jestConfig,
-    moduleNameMapper: {
-        '^lightning/navigation$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
-    }
+    ...jestConfig
 };
