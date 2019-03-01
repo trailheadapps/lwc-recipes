ebd4398d6d2f3a477443205fed0ba3fa8a9d9b1a
muenzpraeger <mail@winkelmeyer.com>
2019-03-01 07:26:36 +0100

    Adding custom platformShowToastEvent mock

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
     moduleNameMapper: {
         '^@salesforce/apex$': '<rootDir>/force-app/test/jest-mocks/apex',
         '^lightning/navigation$':
-            '<rootDir>/force-app/test/jest-mocks/lightning/navigation'
+            '<rootDir>/force-app/test/jest-mocks/lightning/navigation',
+        '^lightning/platformShowToastEvent$':
+            '<rootDir>/force-app/test/jest-mocks/lightning/platformShowToastEvent'
     }
 };
