e257a4e9702fcc5e2418dfe6a1903e3cf056e772
muenzpraeger <mail@winkelmeyer.com>
2019-03-06 03:53:26 +0100

    Issue repro

---
+++ force-app/main/default/lwc/apexImperativeMethodWithApexTypeParams/__tests__/apexImperativeMethodWithParams.test.js
@@ -0,0 +1,146 @@
+import { createElement } from 'lwc';
+import ApexImperativeMethodWithParams from 'c/apexImperativeMethodWithParams';
+import findContacts from '@salesforce/apex/ContactController.findContacts';
+
+// Mocking imperative Apex method call
+jest.mock(
+    '@salesforce/apex/ContactController.findContacts',
+    () => {
+        return {
+            default: jest.fn()
+        };
+    },
+    { virtual: true }
+);
+
+// Sample data for imperative Apex call
+const APEX_CONTACTS_SUCCESS = [
+    {
+        Id: '0031700000pJRRSAA4',
+        Name: 'Amy Taylor',
+        Title: 'VP of Engineering',
+        Phone: '4152568563',
+        Email: 'amy@demo.net',
+        Picture__c:
+            'https://s3-us-west-1.amazonaws.com/sfdc-demo/people/amy_taylor.jpg'
+    }
+];
+
+// Sample error for imperative Apex call
+const APEX_CONTACTS_ERROR = {
+    body: { message: 'An internal server error has occurred' },
+    ok: false,
+    status: 400,
+    statusText: 'Bad Request'
+};
+
+describe('c-apex-imperative-method-with-params', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        // Prevent data saved on mocks from leaking between tests
+        jest.clearAllMocks();
+    });
+
+    // Helper function to wait until the microtask queue is empty. This is needed for promise
+    // timing when calling imperative Apex.
+    function flushPromises() {
+        // eslint-disable-next-line no-undef
+        return new Promise(resolve => setImmediate(resolve));
+    }
+
+    it('passes the user input to the Apex method correctly', () => {
+        const USER_INPUT = 'Taylor';
+        const APEX_PARAMETERS = { searchKey: USER_INPUT };
+
+        // Assign mock value for resolved Apex promise
+        findContacts.mockResolvedValue(APEX_CONTACTS_SUCCESS);
+
+        // Create initial element
+        const element = createElement('c-apex-imperative-method-with-params', {
+            is: ApexImperativeMethodWithParams
+        });
+        document.body.appendChild(element);
+
+        // Select input field for simulating user input
+        const inputEl = element.shadowRoot.querySelector('lightning-input');
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        // Select button for executing Apex call
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return an immediate flushed promise (after the Apex call) to then
+        // wait for any asynchronous DOM updates. Jest will automatically wait
+        // for the Promise chain to complete before ending the test and fail
+        // the test if the promise ends in the rejected state.
+        return flushPromises().then(() => {
+            // Validate parameters of mocked Apex call
+            expect(findContacts.mock.calls[0][0]).toEqual(APEX_PARAMETERS);
+        });
+    });
+
+    it('renders one contact', () => {
+        const USER_INPUT = 'Taylor';
+
+        // Assign mock value for resolved Apex promise
+        findContacts.mockResolvedValue(APEX_CONTACTS_SUCCESS);
+
+        // Create initial element
+        const element = createElement('c-apex-imperative-method-with-params', {
+            is: ApexImperativeMethodWithParams
+        });
+        document.body.appendChild(element);
+
+        // Select input field for simulating user input
+        const inputEl = element.shadowRoot.querySelector('lightning-input');
+        inputEl.value = USER_INPUT;
+        inputEl.dispatchEvent(new CustomEvent('change'));
+
+        // Select button for executing Apex call
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return an immediate flushed promise (after the Apex call) to then
+        // wait for any asynchronous DOM updates. Jest will automatically wait
+        // for the Promise chain to complete before ending the test and fail
+        // the test if the promise ends in the rejected state.
+        return flushPromises().then(() => {
+            // Select div for validating conditionally changed text content
+            const detailEls = element.shadowRoot.querySelectorAll('p');
+            expect(detailEls.length).toBe(APEX_CONTACTS_SUCCESS.length);
+            expect(detailEls[0].textContent).toBe(
+                APEX_CONTACTS_SUCCESS[0].Name
+            );
+        });
+    });
+
+    it('renders the error panel when the Apex method returns an error', () => {
+        // Assing mock value for rejected Apex promise
+        findContacts.mockRejectedValue(APEX_CONTACTS_ERROR);
+
+        // Create initial element
+        const element = createElement('c-apex-imperative-method-with-params', {
+            is: ApexImperativeMethodWithParams
+        });
+        document.body.appendChild(element);
+
+        // Select button for executing Apex call
+        const buttonEl = element.shadowRoot.querySelector('lightning-button');
+        buttonEl.click();
+
+        // Return an immediate flushed promise (after the Apex call) to then
+        // wait for any asynchronous DOM updates. Jest will automatically wait
+        // for the Promise chain to complete before ending the test and fail
+        // the test if the promise ends in the rejected state.
+        return flushPromises().then(() => {
+            const errorPanelEl = element.shadowRoot.querySelector(
+                'c-error-panel'
+            );
+            expect(errorPanelEl).not.toBeNull();
+        });
+    });
+});
