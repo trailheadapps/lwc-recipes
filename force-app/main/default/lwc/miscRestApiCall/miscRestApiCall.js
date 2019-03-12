2044d3db42317156af4d05b9e72ff8e25115c31c
muenzpraeger <mail@winkelmeyer.com>
2019-03-12 16:44:05 +0100

    Add language to search query

--- force-app/main/default/lwc/miscRestApiCall/miscRestApiCall.js
+++ force-app/main/default/lwc/miscRestApiCall/miscRestApiCall.js
@@ -1,7 +1,8 @@
 import { LightningElement, track } from 'lwc';
 
 // The base URL (in this case https://www.googleapis.com/ must be added to the CSP Trusted Sites in Setup)
-const QUERY_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+const QUERY_URL =
+    'https://www.googleapis.com/books/v1/volumes?langRestrict=en&q=';
 
 export default class MiscRestCall extends LightningElement {
     @track searchKey = 'Harry Potter';
