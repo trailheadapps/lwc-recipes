66f6545ac150983f6aa86e65fa0aeebc74c13cec
muenzpraeger <mail@winkelmeyer.com>
2019-03-25 12:27:41 +0100

    Adding Jest tests for miscModal

--- force-app/main/default/lwc/miscModal/miscModal.js
+++ force-app/main/default/lwc/miscModal/miscModal.js
@@ -1,6 +1,6 @@
 import { LightningElement, track } from 'lwc';
 
-export default class MiscSldsModal extends LightningElement {
+export default class MiscModal extends LightningElement {
     @track content = 'The modal content';
     @track header = 'The modal header';
 
