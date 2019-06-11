be7e8862097b2c537503672b9b7d5d2eecaf4e7a
muenzpraeger <mail@winkelmeyer.com>
2019-06-11 06:25:42 +0200

    Prettier

--- bin/check-version.js
+++ bin/check-version.js
@@ -4,9 +4,7 @@ const pjson = require('../package.json');
 const version = pjson.engines.node;
 if (!semver.satisfies(process.version, version)) {
     console.log(
-        `\n\nRequired node version ${version} not satisfied with current version ${
-            process.version
-        }.\n\n`
+        `\n\nRequired node version ${version} not satisfied with current version ${process.version}.\n\n`
     );
     process.exit(1);
 }
