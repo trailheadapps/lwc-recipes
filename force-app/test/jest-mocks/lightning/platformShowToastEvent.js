0ed3f87c72fd0ffce36c0f7345854b2dad3a00c3
Clint Chester <chester.clint@gmail.com>
2019-10-09 15:27:36 +0800

    Documentation updated to point to the correct URL.

--- force-app/test/jest-mocks/lightning/platformShowToastEvent.js
+++ force-app/test/jest-mocks/lightning/platformShowToastEvent.js
@@ -1,7 +1,7 @@
 /**
  * For the original lightning/platformShowToastEvent mock that comes by default with
  * @salesforce/sfdx-lwc-jest, see:
- * https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-mocks/platformShowToastEvent/platformShowToastEvent.js
+ * https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-stubs/platformShowToastEvent/platformShowToastEvent.js
  */
 
 export const ShowToastEventName = 'lightning__showtoast';
