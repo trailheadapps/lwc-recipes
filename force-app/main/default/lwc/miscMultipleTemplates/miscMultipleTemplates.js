6861e218bfd7a1e70972700f102ded7dbe0dfb2b
Jody Bleyle <jbleyle@salesforce.com>
2019-12-10 10:59:50 -0800

    Fix formatting

--- force-app/main/default/lwc/miscMultipleTemplates/miscMultipleTemplates.js
+++ force-app/main/default/lwc/miscMultipleTemplates/miscMultipleTemplates.js
@@ -1,9 +1,15 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import templateOne from './templateOne.html';
 import templateTwo from './templateTwo.html';
 
+import trailheadLogo from '@salesforce/resourceUrl/trailhead_logo';
+import trailheadCharacters from '@salesforce/resourceUrl/trailhead_characters';
+
 export default class MiscMultipleTemplates extends LightningElement {
-    showTemplateOne = true;
+    @track showTemplateOne = true;
+
+    trailheadLogoUrl = trailheadLogo;
+    einsteinUrl = trailheadCharacters + '/images/einstein.png';
 
     render() {
         return this.showTemplateOne ? templateOne : templateTwo;
