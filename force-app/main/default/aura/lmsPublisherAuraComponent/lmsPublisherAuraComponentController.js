57d458cd95b99740ea0e7ea29bff458df5ca406a
Peter Chittum <pchittum@salesforce.com>
2020-03-19 11:33:12 +0000

    initial aura and lwc prototypes

--- force-app/main/default/aura/lmsPublisherAuraComponent/lmsPublisherAuraComponentController.js
+++ force-app/main/default/aura/lmsPublisherAuraComponent/lmsPublisherAuraComponentController.js
@@ -9,7 +9,7 @@
         $A.enqueueAction(action);
     },
     handleClick: function(component, event) {
-        console.dir(event.target.dataset.id);
+        //console.log(event.target.dataset.id);
         const accountId = event.target.dataset.id;
 
         let payload = { recordId: accountId };
