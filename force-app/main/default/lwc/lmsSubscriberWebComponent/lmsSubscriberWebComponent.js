f38be28c3bcbc3f5687881226a93fbd9a66787d9
Peter Chittum <pchittum@salesforce.com>
2020-04-03 12:46:20 +0100

    first prototypes for lwc, aura, and vf page

--- force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
+++ force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
@@ -1,10 +1,45 @@
+/* eslint-disable no-console */
 import { LightningElement, wire } from 'lwc';
-import { /*subscribe,*/ MessageContext } from 'lightning/messageService';
-//import recordSelected from '@salesforce/messageChannel/RecordSelected__c';
+import getContactsByAccountId from '@salesforce/apex/ContactController.getContactsByAccountId';
+import {
+    subscribe,
+    unsubscribe,
+    APPLICATION_SCOPE,
+    MessageContext
+} from 'lightning/messageService';
+import recordSelected from '@salesforce/messageChannel/recordSelected__c';
 
 export default class LmsSubscriberWebComponent extends LightningElement {
-    contacts = [];
+    subscription = null;
+    accountId;
+
+    @wire(getContactsByAccountId, { accountId: '$accountId' })
+    contacts;
 
     @wire(MessageContext)
     messageContext;
+
+    handleSubscribeMC(event) {
+        console.dir(event);
+
+        if (!this.subscription) {
+            this.subscription = subscribe(
+                this.messageContext,
+                recordSelected,
+                message => this.handleMessage(message),
+                { scope: APPLICATION_SCOPE }
+            );
+        }
+    }
+
+    handleUnsubscribeMC(event) {
+        console.dir(event);
+        unsubscribe(this.subscription);
+        this.subscription = null;
+    }
+
+    handleMessage(message) {
+        console.log(`lwc component got record Id: ${message.recordId}`);
+        this.accountId = message.recordId;
+    }
 }
