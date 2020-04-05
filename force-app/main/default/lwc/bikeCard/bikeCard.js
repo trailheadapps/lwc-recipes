1c7513f75a5ae7e8854d4b3c6ba90a219bb10173
Asel <asel.vazirk@gmail.com>
2020-04-05 08:00:26 -0700

    bikeCard lightning component

---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+export default class BikeCard extends LightningElement {
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+ }
