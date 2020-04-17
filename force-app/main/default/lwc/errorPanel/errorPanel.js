65f367b10b17caad49dc3726fb9ed5175acdcc95
Alba Rivas <alba.azconarivas@gmail.com>
2020-04-17 12:24:00 +0200

    Group @api properties at the top

--- force-app/main/default/lwc/errorPanel/errorPanel.js
+++ force-app/main/default/lwc/errorPanel/errorPanel.js
@@ -5,12 +5,11 @@ import NODATA_SVG from '@salesforce/resourceUrl/noDataErrorIllustration';
 export default class ErrorPanel extends LightningElement {
     /** Generic / user-friendly message */
     @api friendlyMessage = 'Error retrieving data';
+    /** Single or array of LDS errors */
+    @api errors;
 
     viewDetails = false;
     noDataSvgUrl = `${NODATA_SVG}#noDataErrorIllustration`;
-
-    /** Single or array of LDS errors */
-    @api errors;
 
     get errorMessages() {
         return reduceErrors(this.errors);
