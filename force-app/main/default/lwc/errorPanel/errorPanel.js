e1e4ca335c6676f5497e18273576a82a72b50609
Alba Rivas <alba.azconarivas@gmail.com>
2020-04-23 12:29:33 +0200

    Small CSS tweak so that the component looks nice in all sample apps (#265)
    * Small CSS tweak so that the component looks nice in all sample apps
    
    * Group @api properties at the top
    
    * Order properties alphabetically

--- force-app/main/default/lwc/errorPanel/errorPanel.js
+++ force-app/main/default/lwc/errorPanel/errorPanel.js
@@ -3,14 +3,13 @@ import { reduceErrors } from 'c/ldsUtils';
 import NODATA_SVG from '@salesforce/resourceUrl/noDataErrorIllustration';
 
 export default class ErrorPanel extends LightningElement {
+    /** Single or array of LDS errors */
+    @api errors;
     /** Generic / user-friendly message */
     @api friendlyMessage = 'Error retrieving data';
 
-    viewDetails = false;
     noDataSvgUrl = `${NODATA_SVG}#noDataErrorIllustration`;
-
-    /** Single or array of LDS errors */
-    @api errors;
+    viewDetails = false;
 
     get errorMessages() {
         return reduceErrors(this.errors);
