644e59af74a699f7d040f16bccd25a4ac1cf692e
Alba Rivas <arivas@salesforce.com>
2020-05-04 17:34:16 +0200

    Include SVG in template so that is can be scaled Use link instead of checkbox to show details Add inline message template

--- force-app/main/default/lwc/errorPanel/errorPanel.js
+++ force-app/main/default/lwc/errorPanel/errorPanel.js
@@ -1,21 +1,28 @@
 import { LightningElement, api } from 'lwc';
 import { reduceErrors } from 'c/ldsUtils';
-import NODATA_SVG from '@salesforce/resourceUrl/noDataErrorIllustration';
+import noDataIllustration from './templates/noDataIllustration.html';
+import inlineMessage from './templates/inlineMessage.html';
 
 export default class ErrorPanel extends LightningElement {
     /** Single or array of LDS errors */
     @api errors;
     /** Generic / user-friendly message */
     @api friendlyMessage = 'Error retrieving data';
+    /** Type of error message **/
+    @api type;
 
-    noDataSvgUrl = `${NODATA_SVG}#noDataErrorIllustration`;
     viewDetails = false;
 
     get errorMessages() {
         return reduceErrors(this.errors);
     }
 
-    handleCheckboxChange(event) {
-        this.viewDetails = event.target.checked;
+    handleShowDetailsClick() {
+        this.viewDetails = !this.viewDetails;
+    }
+
+    render() {
+        if (this.type === 'inlineMessage') return inlineMessage;
+        return noDataIllustration;
     }
 }
