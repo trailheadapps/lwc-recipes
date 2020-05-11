1486772eba07568a88a490db8f0c2f199fb991e1
Peter Chittum <pchittum@salesforce.com>
2020-05-11 08:36:09 +0100

    moves lms lwc subscriber to final use case

--- force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
+++ force-app/main/default/lwc/lmsSubscriberWebComponent/lmsSubscriberWebComponent.js
@@ -1,25 +1,71 @@
 /* eslint-disable no-console */
 import { LightningElement, wire } from 'lwc';
-import getContactsByAccountId from '@salesforce/apex/ContactController.getContactsByAccountId';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { reduceErrors } from 'c/ldsUtils';
+
+// message service and message channel
 import {
     subscribe,
     unsubscribe,
     APPLICATION_SCOPE,
     MessageContext
 } from 'lightning/messageService';
-import recordSelected from '@salesforce/messageChannel/recordSelected__c';
+import recordSelected from '@salesforce/messageChannel/Record_Selected__c';
+
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import TITLE_FIELD from '@salesforce/schema/Contact.Title';
+import PHONE_FIELD from '@salesforce/schema/Contact.Phone';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import PICTURE_FIELD from '@salesforce/schema/Contact.Picture__c';
+
+const fields = [
+    NAME_FIELD,
+    TITLE_FIELD,
+    PHONE_FIELD,
+    EMAIL_FIELD,
+    PICTURE_FIELD
+];
 
 export default class LmsSubscriberWebComponent extends LightningElement {
     subscription = null;
-    accountId;
+    recordId;
 
-    @wire(getContactsByAccountId, { accountId: '$accountId' })
-    contacts;
+    name;
+    title;
+    phone;
+    email;
+    picture;
+
+    @wire(getRecord, { recordId: '$recordId', fields })
+    wiredRecord({ error, data }) {
+        if (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error loading contact',
+                    message: reduceErrors(error).join(', '),
+                    variant: 'error'
+                })
+            );
+        } else if (data) {
+            this.name = getFieldValue(data, NAME_FIELD);
+            this.title = getFieldValue(data, TITLE_FIELD);
+            this.phone = getFieldValue(data, PHONE_FIELD);
+            this.email = getFieldValue(data, EMAIL_FIELD);
+            this.picture = getFieldValue(data, PICTURE_FIELD);
+        }
+    }
 
     @wire(MessageContext)
     messageContext;
 
-    handleSubscribeMC() {
+    // Handler for message received by component
+    handleMessage(message) {
+        this.recordId = message.recordId;
+    }
+
+    // Functions to encapsulate logic for subscription state
+    subscribeToMessageChannel() {
         if (!this.subscription) {
             this.subscription = subscribe(
                 this.messageContext,
@@ -30,13 +76,17 @@ export default class LmsSubscriberWebComponent extends LightningElement {
         }
     }
 
-    handleUnsubscribeMC() {
+    unsubscribeToMessageChannel() {
         unsubscribe(this.subscription);
         this.subscription = null;
     }
 
-    handleMessage(message) {
-        console.log(`lwc component got record Id: ${message.recordId}`);
-        this.accountId = message.recordId;
+    // Standard lifecycle hooks used to sub/unsub to message channel
+    connectedCallback() {
+        this.subscribeToMessageChannel();
+    }
+
+    disconnectedCallback() {
+        this.unsubscribeToMessageChannel();
     }
 }
