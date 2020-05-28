4c9e253b60ff1dcdc46a7d82d124dcbc01e9733e
Peter Chittum <pchittum@salesforce.com>
2020-05-28 13:28:32 +0100

    apply lms recipes to prerelease (#288)
    LMS recipes work. Needs tests.

---
+++ force-app/main/default/aura/lmsPublisherAuraComponent/lmsPublisherAuraComponentController.js
@@ -0,0 +1,16 @@
+({
+    doInit: function (component, event) {
+        var action = component.get('c.getContactList');
+        action.setCallback(this, function (response) {
+            if (response.getState() === 'SUCCESS') {
+                component.set('v.contacts', response.getReturnValue());
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    handleContactSelect: function (component, event) {
+        var payload = { recordId: event.target.contact.Id };
+        // publish LMS message with payload
+        component.find('recordSelected').publish(payload);
+    }
+});
---
+++ force-app/main/default/aura/lmsSubscriberAuraComponent/lmsSubscriberAuraComponentController.js
@@ -0,0 +1,14 @@
+({
+    handleMessage: function (component, message) {
+        if (message && message.getParam('recordId')) {
+            // Retrieve LMS message parameter
+            var recordId = message.getParam('recordId');
+            component.set('v.contactId', recordId);
+            // Refresh record from data service
+            var service = component.find('service');
+            service.reloadRecord();
+        } else {
+            component.set('v.contactId', '');
+        }
+    }
+});
