2af9fd10ee4e4afbb288587e3fa511dc5bf974ec
Alba Rivas <arivas@salesforce.com>
2020-06-23 13:16:29 +0200

    Rename component to be more accurate Reduce code footprint by removing Expected Revenue field and creating a single handler

--- force-app/main/default/lwc/generateRecordInputForCreate/generateRecordInputForCreate.js
+++ force-app/main/default/lwc/generateRecordInputForCreate/generateRecordInputForCreate.js
@@ -9,16 +9,14 @@ import { reduceErrors } from 'c/ldsUtils';
 import ACCOUNT_OBJECT from '@salesforce/schema/Account';
 import NAME_FIELD from '@salesforce/schema/Account.Name';
 import AREANUMBER_FIELD from '@salesforce/schema/Account.AreaNumber__c';
-import EXPECTEDREVENUE_FIELD from '@salesforce/schema/Account.ExpectedRevenue__c';
 
-export default class LdsCreateRecordWithRecordInput extends LightningElement {
+export default class GenerateRecordInputForCreate extends LightningElement {
     areaNumber;
-    areaNumberEditable;
+    areaNumberField = AREANUMBER_FIELD;
+    areaNumberCreateable;
     error;
-    expectedRevenue;
-    expectedRevenueEditable;
     name;
-    nameEditable;
+    nameField = NAME_FIELD;
     recordInput;
 
     @wire(getRecordCreateDefaults, { objectApiName: ACCOUNT_OBJECT })
@@ -32,66 +30,29 @@ export default class LdsCreateRecordWithRecordInput extends LightningElement {
                 recordDefaults,
                 accountObjectInfo // ObjectInfo filters it to only fields that are createable
             );
-            this.setState();
-        } else if (error) {
-            this.setState(error);
-        }
-    }
-
-    setState(error) {
-        if (error) {
-            this.nameEditable = false;
-            this.areaNumberEditable = false;
-            this.expectedRevenueEditable = false;
-            this.name = '';
-            this.areaNumber = '';
-            this.expectedRevenue = '';
-            this.error = error;
-        } else {
             const fields = this.recordInput.fields;
-            this.nameEditable = fields.hasOwnProperty(NAME_FIELD.fieldApiName);
-            this.areaNumberEditable = fields.hasOwnProperty(
-                AREANUMBER_FIELD.fieldApiName
-            );
-            this.expectedRevenueEditable = fields.hasOwnProperty(
-                EXPECTEDREVENUE_FIELD.fieldApiName
-            );
-            this.name = this.nameEditable
-                ? fields[NAME_FIELD.fieldApiName]
-                : '';
-            this.areaNumber = this.areaNumberEditable
-                ? fields[AREANUMBER_FIELD.fieldApiName]
-                : '';
-            this.expectedRevenue = this.expectedRevenueEditable
-                ? fields[EXPECTEDREVENUE_FIELD.fieldApiName]
-                : '';
+            this.areaNumberCreateable = AREANUMBER_FIELD.fieldApiName in fields;
+            this.areaNumber = fields[AREANUMBER_FIELD.fieldApiName];
             this.error = undefined;
+        } else if (error) {
+            this.areaNumberCreateable = undefined;
+            this.areaNumber = undefined;
+            this.name = undefined;
+            this.error = error;
         }
     }
 
-    handleNameChange(event) {
-        this.name = event.target.value;
-    }
-
-    handleAreaNumberChange(event) {
-        this.areaNumber = event.target.value;
-    }
-
-    handleExpectedRevenueChange(event) {
-        this.expectedRevenue = event.target.value;
+    handleFieldChange(event) {
+        this[event.dataset.target.fieldName] = event.target.value;
     }
 
     createAccount() {
-        if (this.nameEditable)
-            this.recordInput.fields[NAME_FIELD.fieldApiName] = this.name;
-        if (this.areaNumberEditable)
+        this.recordInput.fields[NAME_FIELD.fieldApiName] = this.name;
+        if (this.areaNumberCreateable) {
             this.recordInput.fields[
                 AREANUMBER_FIELD.fieldApiName
             ] = this.areaNumber;
-        if (this.expectedRevenueEditable)
-            this.recordInput.fields[
-                EXPECTEDREVENUE_FIELD.fieldApiName
-            ] = this.expectedRevenue;
+        }
         createRecord(this.recordInput)
             .then((account) => {
                 this.dispatchEvent(
