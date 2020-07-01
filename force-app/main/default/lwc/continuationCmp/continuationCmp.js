6b6d2edc8746ea8092e0c385ef3aec1e034f8750
Jag <jjayaprakash@salesforce.com>
2020-07-01 14:12:52 -0700

    A sample apex-continuation component

---
+++ force-app/main/default/lwc/continuationCmp/continuationCmp.js
@@ -0,0 +1,29 @@
+import { LightningElement, track, wire } from 'lwc';
+import startRequest from '@salesforce/apexContinuation/SampleContinuationClass.startRequest';
+export default class ContinuationComponent extends LightningElement {
+    @track imperativeContinuation = {};
+
+    // Using wire service
+    @wire(startRequest)
+    wiredContinuation;
+
+    get formattedWireResult() {
+        return JSON.stringify(this.wiredContinuation);
+    }
+
+    // Imperative Call
+    callContinuation() {
+        startRequest()
+            .then(result => {
+                this.imperativeContinuation = result;
+            })
+            .catch(error => {
+                this.imperativeContinuation = error;
+            }
+        );
+    }
+
+    get formattedImperativeResult() {
+        return JSON.stringify(this.imperativeContinuation);
+    }
+}
