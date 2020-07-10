8536b316536419c8728b8704f67e4e21306de36f
Peter Chittum <pchittum@salesforce.com>
2020-07-10 12:36:47 +0100

    added features for permission-based recipe

---
+++ force-app/main/default/lwc/miscPermissionBasedUI/miscPermissionBasedUI.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+// import the permission from userPermission (standard Salesforce permissions) or
+// customPermission (custom org-defined permissions)
+import hasAccessRestrictedUI from '@salesforce/customPermission/accessRestrictedUIPermission';
+
+export default class MiscPermissionBasedUI extends LightningElement {
+    // surface imported permission to HTML template
+    isRestrictedUIAccessible = hasAccessRestrictedUI;
+}
