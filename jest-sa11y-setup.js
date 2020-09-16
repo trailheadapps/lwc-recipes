417777b1c34c4a21368246a997b527cb0153557b
Alba Rivas <arivas@salesforce.com>
2020-09-16 10:35:36 +0200

    Remove BSD-3 header

--- jest-sa11y-setup.js
+++ jest-sa11y-setup.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: BSD-3-Clause
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
-
 // Since this is not a typescript project, disabling typescript eslint warning
 // TODO (lint): using 'exclude: ["./packages/test-integration/**/*.js"]' in tsconfig.eslint.json doesn't work
 // eslint-disable-next-line @typescript-eslint/no-var-requires
