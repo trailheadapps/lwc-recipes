57589230db61463bdd856095a2bb2788283eb44f
Alba Rivas <arivas@salesforce.com>
2020-09-24 13:21:13 +0200

    Update setup file

--- jest-sa11y-setup.js
+++ jest-sa11y-setup.js
@@ -1,6 +1,3 @@
-// Since this is not a typescript project, disabling typescript eslint warning
-// TODO (lint): using 'exclude: ["./packages/test-integration/**/*.js"]' in tsconfig.eslint.json doesn't work
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { registerSa11yMatcher } = require('@sa11y/jest');
+import { registerSa11yMatcher } from '@sa11y/jest';
 
 registerSa11yMatcher();
