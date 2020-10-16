a3fbc038fac8748bfd22ccaabed86978e5cbb002
Peter Chittum <pchittum@salesforce.com>
2020-10-16 11:24:20 +0100

    handle null element in modal hide

--- force-app/main/default/lwc/modal/modal.js
+++ force-app/main/default/lwc/modal/modal.js
@@ -32,11 +32,21 @@ export default class Modal extends LightningElement {
     }
 
     handleSlotTaglineChange() {
+        // Only needed in "show" state. If hiding, we're removing from DOM anyway
+        // Added to address Issue #344 where querySelector would intermittently return null element on hide
+        if (this.showModal === false) {
+            return;
+        }
         const taglineEl = this.template.querySelector('p');
         taglineEl.classList.remove(CSS_CLASS);
     }
 
     handleSlotFooterChange() {
+        // Only needed in "show" state. If hiding, we're removing from DOM anyway
+        // Added to address Issue #344 where querySelector would intermittently return null element on hide
+        if (this.showModal === false) {
+            return;
+        }
         const footerEl = this.template.querySelector('footer');
         footerEl.classList.remove(CSS_CLASS);
     }
