136b29711cf6cac011911684e0b6293d0e4004dd
ruwanthalk@gmail.com <ruwanthalk@gmail.com>
2020-11-15 18:21:07 +0530

    get the selected users to the JS controller

--- force-app/main/default/lwc/userResutTable/userResutTable.js
+++ force-app/main/default/lwc/userResutTable/userResutTable.js
@@ -1,10 +1,21 @@
-import { api, LightningElement } from 'lwc';
+import { api, LightningElement, track } from 'lwc';
 
 export default class UserResutTable extends LightningElement {
 
 
     @api users;
+    @track selectedUsers = [];
 
 
+    toggleUserSelect(event) {
+        console.log(this.users);
+        console.log(event.target.dataset.userId);
 
+
+    }
+
+    passToQueueAssignment(event) {
+
+
+    }
 }
