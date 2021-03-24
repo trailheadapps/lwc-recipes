b551d6dd5fc35ed7f136753805c5e82f0792efa3
Alexander <bykmakep@gmail.com>
2021-03-24 20:44:23 +0200

    Update ldsUtils.js
    I'm not sure if my proposed change is the best approach, please let me know if there is a better approach.
    
    The original function was not able to to reduce this error properly - { "status": 500, "body": { "fieldErrors": {}, "pageErrors": [{ "statusCode": "FIELD_CUSTOM_VALIDATION_EXCEPTION", "message": "Validation error on Contact: Please select the Salutation" }], "index": null, "duplicateResults": [] }, "headers": {} }
    
    It was returning "Server Error" instead of "Validation error on Contact: Please select the Salutation". The original code was not expecting that pageErrors will be inside error.body

--- force-app/main/default/lwc/ldsUtils/ldsUtils.js
+++ force-app/main/default/lwc/ldsUtils/ldsUtils.js
@@ -8,30 +8,35 @@ export function reduceErrors(errors) {
         errors = [errors];
     }
 
-    return (
-        errors
-            // Remove null/undefined items
-            .filter((error) => !!error)
-            // Extract an error message
-            .map((error) => {
-                // UI API read errors
-                if (Array.isArray(error.body)) {
-                    return error.body.map((e) => e.message);
-                }
-                // UI API DML, Apex and network errors
-                else if (error.body && typeof error.body.message === 'string') {
-                    return error.body.message;
-                }
-                // JS errors
-                else if (typeof error.message === 'string') {
-                    return error.message;
-                }
-                // Unknown error shape so try HTTP status text
-                return error.statusText;
-            })
-            // Flatten
-            .reduce((prev, curr) => prev.concat(curr), [])
-            // Remove empty strings
-            .filter((message) => !!message)
-    );
+        return (
+            errors
+                // Remove null/undefined items
+                .filter((error) => !!error)
+                // Extract an error message
+                .map((error) => {
+                    // UI API read errors
+                    if (Array.isArray(error.body)) {
+                        return error.body.map((e) => e.message);
+                    }
+                    // UI API DML, Apex and network errors
+                    else if (error.body) {
+                        if (typeof error.body.message === 'string') {
+                            return error.body.message;
+                        }
+                        else if (error.body.pageErrors) {
+                            return error.body.pageErrors.map((e) => e.message);
+                        }
+                    }
+                    // JS errors
+                    else if (typeof error.message === 'string') {
+                        return error.message;
+                    }
+                    // Unknown error shape so try HTTP status text
+                    return error.statusText;
+                })
+                // Flatten
+                .reduce((prev, curr) => prev.concat(curr), [])
+                // Remove empty strings
+                .filter((message) => !!message)
+        );
 }
