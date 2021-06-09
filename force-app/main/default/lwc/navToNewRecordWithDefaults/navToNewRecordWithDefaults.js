42cf4efd715625e66094ff666575dcf3a94d484e
Philippe Ozil <5071767+pozil@users.noreply.github.com>
2021-06-09 10:32:56 +0200

    Fixed NavToNewRecordWithDefaults test (#511)

--- force-app/main/default/lwc/navToNewRecordWithDefaults/navToNewRecordWithDefaults.js
+++ force-app/main/default/lwc/navToNewRecordWithDefaults/navToNewRecordWithDefaults.js
@@ -1,18 +1,16 @@
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
-// currently the below import is not supported with sfdx-lwc-jest v0.7.0
 import { encodeDefaultFieldValues } from 'lightning/pageReferenceUtils';
 
-export default class NavToNewRecord extends NavigationMixin(LightningElement) {
+export default class NavToNewRecordWithDefaults extends NavigationMixin(
+    LightningElement
+) {
     navigateToNewContactWithDefaults() {
         const defaultValues = encodeDefaultFieldValues({
             FirstName: 'Morag',
             LastName: 'de Fault',
             LeadSource: 'Other'
         });
-
-        // eslint-disable-next-line no-console
-        console.log(defaultValues);
 
         this[NavigationMixin.Navigate]({
             type: 'standard__objectPage',
