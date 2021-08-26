b8bb418d86125fb2cca0d6ed4ec352ec218b76bd
adityanaag3 <atopalli@salesforce.com>
2021-08-26 15:48:52 +0530

    Added error handling for validation rules

--- force-app/main/default/lwc/ldsUtils/ldsUtils.js
+++ force-app/main/default/lwc/ldsUtils/ldsUtils.js
@@ -18,6 +18,42 @@ export function reduceErrors(errors) {
                 if (Array.isArray(error.body)) {
                     return error.body.map((e) => e.message);
                 }
+                // Errors from page level errors
+                else if (
+                    error.body &&
+                    error.body.pageErrors &&
+                    error.body.pageErrors.length > 0
+                ) {
+                    return error.body.pageErrors.map((e) => e.message);
+                }
+                // Errors from field level errors
+                else if (
+                    error.body &&
+                    error.body.fieldErrors &&
+                    Object.keys(error.body.fieldErrors).length > 0
+                ) {
+                    const fieldErrors = [];
+                    Object.values(error.body.fieldErrors).forEach(
+                        (errorArray) => {
+                            fieldErrors.push(
+                                ...errorArray.map((e) => e.message)
+                            );
+                        }
+                    );
+                    return fieldErrors;
+                }
+                // Errors from UI API DML page level errors
+                else if (
+                    error.body &&
+                    error.body.output &&
+                    error.body.output.errors &&
+                    error.body.output.errors.length > 0
+                ) {
+                    return error.body.output.errors.map((e) => e.message);
+                }
+                /* 
+                PR 556 - Errors from UI API DML field level errors - PR 556
+                */
                 // UI API DML, Apex and network errors
                 else if (error.body && typeof error.body.message === 'string') {
                     return error.body.message;
