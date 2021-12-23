01aa8c016ee8a35249849209387d8506a2c5d55d
Philippe Ozil <philippe.ozil@gmail.com>
2021-12-23 09:45:32 +0100

    Minor fixes (#610)
    * fix: simplified CSS selector
    
    * Removed duplicate calls to jest.useFakeTimers()
    
    * Fixed package-lock.json CI issue
    
    * Applied prettier (incorrect version)

--- force-app/main/default/lwc/libsD3/__tests__/libsD3.test.js
+++ force-app/main/default/lwc/libsD3/__tests__/libsD3.test.js
@@ -36,7 +36,7 @@ describe('c-libs-d3', () => {
         document.body.appendChild(element);
 
         // Querying the DOM element that has the lwc:dom directive set.
-        const domEl = element.shadowRoot.querySelector('svg[class="d3"]');
+        const domEl = element.shadowRoot.querySelector('svg.d3');
         expect(domEl).not.toBeNull();
     });
 
