62cc0dc70338a54c710b87f1eccd1c0591097080
Filip Rafałowicz <f.rafalowicz@polsource.com>
2022-02-01 14:04:48 +0100

    Updated mock for message service to work correctly when multiple tests use it at the same time

--- force-app/test/jest-mocks/lightning/messageService.js
+++ force-app/test/jest-mocks/lightning/messageService.js
@@ -8,16 +8,38 @@ export const createMessageChannel = jest.fn();
 export const createMessageContext = jest.fn();
 export const MessageContext = jest.fn();
 export const releaseMessageContext = jest.fn();
-export const unsubscribe = jest.fn();
 
 // Assigns a handler for each channel subscribed, so that multiple channels can be subscribed to
 // within the same test execution context
 const handlers = {};
+
 export const publish = jest.fn((messageContext, messageChannel, message) => {
-    handlers[messageChannel]?.(message);
+    handlers[messageChannel]?.forEach((handlerObj) =>
+        handlerObj.handler(message)
+    );
 });
+
 export const subscribe = jest.fn(
     (messageContext, messageChannel, messageHandler) => {
-        handlers[messageChannel] = messageHandler;
+        const subscriptionId = crypto.randomUUID();
+
+        if (!handlers[messageChannel]) {
+            handlers[messageChannel] = [];
+        }
+
+        handlers[messageChannel].push({
+            id: subscriptionId,
+            handler: messageHandler
+        });
+
+        return { id: subscriptionId };
     }
 );
+
+export const unsubscribe = jest.fn((subscription) => {
+    Object.keys(handlers).forEach((messageChannel) => {
+        handlers[messageChannel] = handlers[messageChannel].filter(
+            (handler) => handler.id !== subscription.id
+        );
+    });
+});
---
+++ jest-crypto-setup.js
@@ -0,0 +1,2 @@
+const crypto = require('crypto');
+global.crypto = crypto;
