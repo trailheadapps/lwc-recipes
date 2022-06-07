3454a672ea190f9466ba03ae662be1896f7f80e1
satyasekharcvb <ssekhar@salesforce.com>
2022-06-07 14:31:34 +0530

    Changes requested in the review - drop lightDOMHello component and copyright header in test mocks

--- force-app/test/jest-mocks/lightning/prompt.js
+++ force-app/test/jest-mocks/lightning/prompt.js
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2018, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
 import { LightningElement, api } from 'lwc';
 
 export default class Prompt extends LightningElement {
--- force-app/test/jest-mocks/lightning/uiRecordApi.js
+++ force-app/test/jest-mocks/lightning/uiRecordApi.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2018, salesforce.com, inc.
- * All rights reserved.
- * SPDX-License-Identifier: MIT
- * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
- */
-
 import { createLdsTestWireAdapter } from '@salesforce/wire-service-jest-util';
 export const getRecord = createLdsTestWireAdapter(jest.fn());
 export const getRecordCreateDefaults = createLdsTestWireAdapter(jest.fn());
