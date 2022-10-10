96dda5a88cf0e522073936a11a6fef4954b052e1
Adam Reineke <adam@reineke.me>
2022-10-10 00:27:19 -0700

    Make the lightning/navigation mock resettable (#721)
    * add identical tests to demonstrate bug
    
    * update navigation mock to be resettable
    
    * add a mock reset to all tests with navigation mock
    
    * remove proof of concept tests that now both pass
    
    * two minor changes
    
    * Use the `lastCall` jest property
    
    * comment edit for clarity

--- force-app/main/default/lwc/navToNewRecord/__tests__/navToNewRecord.test.js
+++ force-app/main/default/lwc/navToNewRecord/__tests__/navToNewRecord.test.js
@@ -11,6 +11,8 @@ describe('c-nav-to-new-record', () => {
         while (document.body.firstChild) {
             document.body.removeChild(document.body.firstChild);
         }
+        // Reset the navigation mock between tests
+        jest.clearAllMocks();
     });
 
     // Helper function to wait until the microtask queue is empty. This is needed for promise
--- force-app/test/jest-mocks/lightning/navigation.js
+++ force-app/test/jest-mocks/lightning/navigation.js
@@ -7,18 +7,22 @@
 import { createTestWireAdapter } from '@salesforce/wire-service-jest-util';
 export const CurrentPageReference = createTestWireAdapter(jest.fn());
 
-let _navigatePageReference, _generatePageReference, _replace;
-
 const Navigate = Symbol('Navigate');
 const GenerateUrl = Symbol('GenerateUrl');
+
+// We need the mock to reset between tests, so we hold the state in jest.fn() objects.
+// We use Jest's reflection methods to implement the two CalledWith helpers below so we don't need to update all the existing tests.
+// A cleaner implementation would just export these two mocks directly for consumption by the test authors to verify the state.
+export const mockNavigate = jest.fn();
+export const mockGenerate = jest.fn();
+
 export const NavigationMixin = (Base) => {
     return class extends Base {
         [Navigate](pageReference, replace) {
-            _navigatePageReference = pageReference;
-            _replace = replace;
+            mockNavigate({ pageReference, replace });
         }
         [GenerateUrl](pageReference) {
-            _generatePageReference = pageReference;
+            mockGenerate({ pageReference });
             return new Promise((resolve) => resolve('https://www.example.com'));
         }
     };
@@ -32,12 +36,32 @@ NavigationMixin.GenerateUrl = GenerateUrl;
  * invoked with and provide access with this function.
  */
 export const getNavigateCalledWith = () => {
-    return {
-        pageReference: _navigatePageReference,
-        replace: _replace
-    };
+    // If the mock was never called, return the object with undefined properties.
+    // This prevents exceptions when tests destructure this object, allowing them to
+    // fail in more expected ways as the test verifies properties on the pageReference object.
+    if (mockNavigate.mock.calls.length === 0) {
+        return {
+            pageReference: undefined,
+            replace: undefined
+        };
+    }
+
+    // The mock was called, so return the most recent last call.
+    // Because the mock is called with a single object, it's at the zero index.
+    return mockNavigate.mock.lastCall[0];
 };
 
-export const getGenerateUrlCalledWith = () => ({
-    pageReference: _generatePageReference
-});
+export const getGenerateUrlCalledWith = () => {
+    // If the mock was never called, return the object with undefined properties.
+    // This prevents exceptions when tests destructure this object, allowing them to
+    // fail in more expected ways as the test verifies properties on the pageReference object.
+    if (mockGenerate.mock.calls.length === 0) {
+        return {
+            pageReference: undefined
+        };
+    }
+
+    // The mock was called, so return the most recent last call.
+    // Because the mock is called with a single object, it's at the zero index.
+    return mockGenerate.mock.lastCall[0];
+};
