081ef7ad7d09617568e8b880ba25516c1a6d3525
satyasekharcvb <ssekhar@salesforce.com>
2023-02-08 19:06:31 +0530

    added error test case

--- force-app/main/default/lwc/ldsNotifyRecordUpdateAvailable/__tests__/ldsNotifyRecordUpdateAvailable.test.js
+++ force-app/main/default/lwc/ldsNotifyRecordUpdateAvailable/__tests__/ldsNotifyRecordUpdateAvailable.test.js
@@ -1,13 +1,13 @@
 import { createElement } from 'lwc';
 import LdsNotifyRecordUpdateAvailable from 'c/ldsNotifyRecordUpdateAvailable';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
 import updateContact from '@salesforce/apex/ContactController.updateContact';
 import { ShowToastEventName } from 'lightning/platformShowToastEvent';
 
 // Mock realistic data
 const mockGetRecord = require('./data/getRecord.json');
 
-//Mock updateContacts
+//Mock updateContact
 jest.mock(
     '@salesforce/apex/ContactController.updateContact',
     () => {
@@ -18,7 +18,15 @@ jest.mock(
     { virtual: true }
 );
 
-describe('c-notify-contact-update-available', () => {
+// Sample error for Apex call
+const UPDATE_CONTACT_ERROR = {
+    body: { message: 'An internal server error has occurred' },
+    ok: false,
+    status: 400,
+    statusText: 'Bad Request'
+};
+
+describe('c-lds-notify-record-update-available', () => {
     afterEach(() => {
         // The jsdom instance is shared across test cases in a single file so reset the DOM
         while (document.body.firstChild) {
@@ -92,11 +100,58 @@ describe('c-notify-contact-update-available', () => {
         // Return a promise to wait for any asynchronous DOM updates.
         return Promise.resolve()
             .then(() => {
+                //Validate updateContact has been called
                 expect(updateContact).toHaveBeenCalledTimes(1);
+                //Validate updateContact is called with correct parameters
+                expect(updateContact.mock.calls[0]).toEqual([
+                    { firstName: 'John', lastName: 'Doe' }
+                ]);
+                //Validate notifyRecordUpdateAvailable is called
+                expect(notifyRecordUpdateAvailable).toHaveBeenCalledTimes(1);
             })
             .then(() => {
+                //Validate success toast handler is called
                 expect(toastHandler).toHaveBeenCalledTimes(1);
+                expect(toastHandler.mock.calls[0][0].detail.variant).toBe(
+                    'success'
+                );
             });
+    });
+
+    it('displays an error toast on update record error', async () => {
+        // Create element
+        const element = createElement('c-lds-notify-record-update-available', {
+            is: LdsNotifyRecordUpdateAvailable
+        });
+        document.body.appendChild(element);
+
+        // Mock handler for toast event
+        const toastHandler = jest.fn();
+        // Add event listener to catch toast event
+        element.addEventListener(ShowToastEventName, toastHandler);
+
+        // Emit data from @wire
+        getRecord.emit(mockGetRecord);
+
+        // Wait for any asynchronous DOM updates
+        await flushPromises();
+
+        // Assign mock value for rejected updateContacts promise
+        updateContact.mockRejectedValue(UPDATE_CONTACT_ERROR);
+
+        // Find the save button and click
+        const inputEl = element.shadowRoot.querySelector('lightning-button');
+        inputEl.click();
+
+        // Wait for any asynchronous DOM updates
+        await flushPromises();
+
+        // Return a promise to wait for any asynchronous DOM updates.
+        return Promise.resolve().then(() => {
+            //Validate success toast handler is called
+            expect(toastHandler).toHaveBeenCalledTimes(1);
+            expect(toastHandler.mock.calls[0][0].detail.variant).toBe('error');
+        });
     });
 
     it('is accessible when data is returned', async () => {
