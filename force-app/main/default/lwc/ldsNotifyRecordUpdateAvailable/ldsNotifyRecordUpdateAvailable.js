3c8fd49b81deaee6d1c72a0020e714a46b40f5b1
satyasekharcvb <31529847+satyasekharcvb@users.noreply.github.com>
2023-02-08 20:05:30 +0530

    Added new recipe for notifyRecordUpdateAvailable (#771)
    * ldsNotifyRecordUpdateAvailable Recipe
    
    * Fixed test script
    
    * added error test case
    
    * fixed typos in the testscript comments
    
    ---------
    
    Co-authored-by: adityanaag3 <atopalli@salesforce.com>

---
+++ force-app/main/default/lwc/ldsNotifyRecordUpdateAvailable/ldsNotifyRecordUpdateAvailable.js
@@ -0,0 +1,55 @@
+import { LightningElement, api, wire } from 'lwc';
+import updateContact from '@salesforce/apex/ContactController.updateContact';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import {
+    getRecord,
+    getFieldValue,
+    notifyRecordUpdateAvailable
+} from 'lightning/uiRecordApi';
+
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+
+const fields = [FIRSTNAME_FIELD, LASTNAME_FIELD];
+
+export default class LdsNotifyRecordUpdateAvailable extends LightningElement {
+    @api recordId;
+
+    @wire(getRecord, { recordId: '$recordId', fields })
+    contact;
+
+    get firstName() {
+        return getFieldValue(this.contact.data, FIRSTNAME_FIELD);
+    }
+
+    get lastName() {
+        return getFieldValue(this.contact.data, LASTNAME_FIELD);
+    }
+
+    handleContactUpdate() {
+        updateContact({
+            recordId: this.recordId,
+            firstName: this.refs.firstName.value,
+            lastName: this.refs.lastName.value
+        })
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Contact updated',
+                        variant: 'success'
+                    })
+                );
+                notifyRecordUpdateAvailable([{ recordId: this.recordId }]);
+            })
+            .catch((error) => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error updating record',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+            });
+    }
+}
