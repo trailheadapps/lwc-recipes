03c2d745ad60c9aba18527edb0c9d891f2ec4bf3
Mike Burr <mburr@salesforce.com>
2023-05-09 17:11:46 -0400

    updates from review comments

--- force-app/main/default/lwc/graphqlPagination/graphqlPagination.js
+++ force-app/main/default/lwc/graphqlPagination/graphqlPagination.js
@@ -1,18 +1,18 @@
 import { LightningElement, wire } from 'lwc';
 import { gql, graphql } from 'lightning/uiGraphQLApi';
 
-/** The delay used when debouncing event handlers before invoking Apex. */
-const DELAY = 300;
+const pageSize = 3;
 
 export default class GraphqlPagination extends LightningElement {
     after;
+    pageNumber = 1;
 
     @wire(graphql, {
         query: gql`
-            query searchContacts ($after: String = null) {
+            query searchContacts ($after: String, $pageSize: Int!) {
                 uiapi {
                     query {
-                        Contact (first: 3,
+                        Contact (first: $pageSize,
                                  after: $after,
                                  orderBy: { Name: { order: ASC } }) {
                             edges {
@@ -26,7 +26,9 @@ export default class GraphqlPagination extends LightningElement {
                             pageInfo {
                                 endCursor
                                 hasNextPage
+                                hasPreviousPage
                             }
+                            totalCount
                         }
                     }
                 }
@@ -37,20 +39,42 @@ export default class GraphqlPagination extends LightningElement {
     contacts;
 
     get variables() {
-        return { after: this.after || null };
+        return {
+            after: this.after || null,
+            pageSize
+        };
     }
 
-    get nextDisabled() {
-        return ! this.contacts.data?.uiapi.query.Contact.pageInfo.hasNextPage;
+    get currentPageNumber() {
+        return this.totalCount === 0 ? 0 : this.pageNumber;
+    }
+
+    get isNotFirstPage() {
+        return this.contacts.data?.uiapi.query.Contact.pageInfo.hasPreviousPage;
+        
+    }
+
+    get isNotLastPage() {
+        return this.contacts.data?.uiapi.query.Contact.pageInfo.hasNextPage;
+    }
+
+    get totalCount() {
+        return this.contacts.data?.uiapi.query.Contact.totalCount || 0;
+    }
+
+    get totalPages() {
+        return Math.ceil(this.totalCount / pageSize);
+    }
+
+    handleNext(event) {
+        if (this.contacts.data?.uiapi.query.Contact.pageInfo.hasNextPage) {
+            this.after = this.contacts.data.uiapi.query.Contact.pageInfo.endCursor;
+            this.pageNumber++;
+        }
     }
 
     handleReset(event) {
-        this.after = undefined;
-    }
-
-    handleNextPage(event) {
-        if (this.contacts.data?.uiapi.query.Contact.pageInfo.hasNextPage) {
-            this.after = this.contacts.data.uiapi.query.Contact.pageInfo.endCursor;
-        }
+        this.after = null;
+        this.pageNumber = 1;
     }
 }
