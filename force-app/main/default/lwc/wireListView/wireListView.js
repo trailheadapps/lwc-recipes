57d248d238ad25fa9876c61aec0ea00b86920e58
spencer-brooks-sf <42548303+spencer-brooks-sf@users.noreply.github.com>
2023-06-27 09:18:39 -0400

    docs: clarify listViewApiName needs updated if not using recipe org

--- force-app/main/default/lwc/wireListView/wireListView.js
+++ force-app/main/default/lwc/wireListView/wireListView.js
@@ -5,6 +5,7 @@ import CONTACT_OBJECT from '@salesforce/schema/Contact';
 import NAME_FIELD from '@salesforce/schema/Contact.Name';
 
 export default class WireListView extends LightningElement {
+    // Update listViewApiName to be a Contact list view dev name on your org if not using the recipe org
     @wire(getListUi, {
         objectApiName: CONTACT_OBJECT,
         listViewApiName: 'All_Recipes_Contacts',
