8fd282111021babdb67a31226550e971e4e3ed27
skempf <skempf@salesforce.com>
2023-10-10 19:06:09 +0200

    feat: adding new recordPicker cmp for basic use case

---
+++ force-app/main/default/lwc/recordPickerHello/__tests__/recordPickerHello.test.js
@@ -0,0 +1,68 @@
+import { createElement } from 'lwc';
+import RecordPickerHello from 'c/recordPickerHello';
+import { graphql } from 'lightning/uiGraphQLApi';
+
+// Mock realistic data
+// import mockGraphQL from '../data/graphqlContactResult.json';
+const mockGraphQL = require('../data/graphqlContactResult.json');
+
+describe('recordPickerHello', () => {
+    let element;
+    beforeEach(() => {
+        // Create component
+        element = createElement('c-record-picker-hello', {
+            is: RecordPickerHello
+        });
+        element.objectApiName = 'Contact';
+        element.label = 'Contact';
+        document.body.appendChild(element);
+    });
+
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+        // Prevent data saved on mocks from leaking between tests
+        jest.clearAllMocks();
+    });
+
+    // Helper function to wait until the microtask queue is empty. This is needed for promise
+    // timing when calling imperative Apex.
+    async function flushPromises() {
+        return Promise.resolve();
+    }
+
+    it('renders a lightning-record-picker component', () => {
+        const recordPickerElement = element.shadowRoot.querySelector(
+            'lightning-record-picker'
+        );
+        expect(recordPickerElement).toBeTruthy();
+    });
+
+    it('is accessible', async () => {
+        expect(element).toBeAccessible();
+    });
+
+    it('changes the target object of the record picker', async () => {
+        // set selected record
+        const recordPickerElement = element.shadowRoot.querySelector(
+            'lightning-record-picker'
+        );
+        recordPickerElement.value = '003Z70000016iOUIAY';
+        recordPickerElement.dispatchEvent(new CustomEvent('change'));
+        // Emit data from @wire
+        graphql.emit(mockGraphQL);
+
+        // Wait for any asynchronous DOM updates
+        await flushPromises();
+        const selectedRecordDetails = element.shadowRoot.querySelector(
+            'p.selectedRecordDetails'
+        );
+        await flushPromises();
+
+        console.log(selectedRecordDetails);
+
+        expect(selectedRecordDetails).toBeTruthy();
+    });
+});
---
+++ force-app/main/default/lwc/recordPickerHello/recordPickerHello.js
@@ -0,0 +1,100 @@
+import { wire, LightningElement } from 'lwc';
+import { getObjectInfos } from 'lightning/uiObjectInfoApi';
+import { gql, graphql } from 'lightning/uiGraphQLApi';
+
+export default class RecordPickerHello extends LightningElement {
+    placeholder = 'Search';
+
+    label = 'Record Picker';
+
+    currentObjectApiName = 'Contact';
+    objectInfos = [];
+
+    selectedRecordId = '';
+
+    handleChange(event) {
+        this.selectedRecordId = event.target.recordId;
+    }
+
+    contact = {};
+
+    get variables() {
+        return {
+            selectedRecordId: this.selectedRecordId
+        };
+    }
+
+    @wire(graphql, {
+        query: gql`
+            query searchContacts($selectedRecordId: ID) {
+                uiapi {
+                    query {
+                        Contact(
+                            where: { Id: { eq: $selectedRecordId } }
+                            first: 1
+                        ) {
+                            edges {
+                                node {
+                                    Id
+                                    Name {
+                                        value
+                                    }
+                                    Phone {
+                                        value
+                                        displayValue
+                                    }
+                                    Title {
+                                        value
+                                        displayValue
+                                        label
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        `,
+        variables: '$variables'
+    })
+    wiredGraphQL({ data, errors }) {
+        if (errors) {
+            this.dispatchEvent(new CustomEvent('error', { error: errors }));
+            return;
+        }
+
+        if (!data) {
+            return;
+        }
+
+        this.contact = data.uiapi.query.Contact.edges.map(
+            (edge) =>
+                ({
+                    Id: edge.node.Id,
+                    Name: edge.node.Name.value,
+                    Phone: edge.node.Phone.value,
+                    Picture__c: edge.node.Picture__c.value,
+                    Title: edge.node.Title.value
+                })[0]
+        );
+    }
+
+    @wire(getObjectInfos, { objectApiNames: '$objectApiNames' })
+    wiredGetObjectInfos({ error, data }) {
+        if (error) {
+            this.dispatchEvent(new CustomEvent('error', { error: error }));
+            return;
+        }
+
+        if (!data) {
+            return;
+        }
+
+        this.objectInfos = [];
+        data.results.forEach((result) => {
+            const objectInfo = result.result;
+            this.objectInfos.push(objectInfo);
+        });
+        this.isObjectInfoLoading = false;
+    }
+}
