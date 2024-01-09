44f0b915b454da49b1e06eb7ddbf9844711602a9
Antoine Rosenbach <arosenbach@salesforce.com>
2024-01-09 15:25:59 +0100

    chore: add comments + error and view source panel

--- force-app/main/default/lwc/recordPickerMultiValue/recordPickerMultiValue.js
+++ force-app/main/default/lwc/recordPickerMultiValue/recordPickerMultiValue.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire, track } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import CONTACT_NAME_FIELD from '@salesforce/schema/Contact.Name';
 
@@ -13,71 +13,88 @@ const toContactPill = (record) => ({
     iconName: 'standard:contact'
 });
 
+// Converts a list a IDs to lightning-record-picker filter
 const toRecordPickerFilter = (ids) => ({
     criteria: [
         {
             fieldPath: 'Id',
-            operator: 'nin',
+            operator: 'nin', // "not in" operator
             value: ids
         }
     ]
 });
 
 export default class RecordPickerMultiValue extends LightningElement {
-    lastSelectedRecordId;
+    /**
+     * The last selected record ID.
+     * Used to trigger getRecord @wire calls.
+     */
+    recordId;
 
+    /**
+     * The list of selected records (id and name).
+     * Used to compute and update the list of pill items
+     * and to update the record picker filter
+     * as we want to filter out already selected records from the record picker suggestions
+     */
     selectedRecords = [];
 
-    @track
-    pillItems;
-
-    @track
-    recordPickerFilter;
-
-    handleRecordPickerChange(event) {
-        this.lastSelectedRecordId = event.detail.recordId;
+    /**
+     * Builds lightning-pill items from `selectedRecords`
+     */
+    get pillItems() {
+        return this.selectedRecords.map(toContactPill);
     }
 
+    /**
+     * Builds lightning-record-picker filter from `selectedRecords`
+     */
+    get recordPickerFilter() {
+        // Convert selectedRecords to a list of IDs
+        const selectedRecordIds = this.selectedRecords.map(
+            (record) => record.id
+        );
+        return toRecordPickerFilter(selectedRecordIds);
+    }
+
+    /**
+     * getRecord wire adapter is used here to get the name of the records
+     * that are selected in the record picker.
+     * Note: GraphQL could also be used (see recordPickerHello.js)
+     */
     @wire(getRecord, {
-        recordId: '$lastSelectedRecordId',
+        recordId: '$recordId',
         fields: [CONTACT_NAME_FIELD]
     })
     wiredGetRecord({ data, error }) {
+        this.wireError = error;
         if (error || !data) {
             return;
         }
 
-        const recordId = this.lastSelectedRecordId;
-
         this.selectedRecords = [
             ...this.selectedRecords,
             {
-                id: recordId,
+                id: data.id,
                 name: data.fields[CONTACT_NAME_FIELD.fieldApiName].value
             }
         ];
-        this._updatePillItems();
-        this._updateRecordPickerFilter();
-
-        this.refs.recordPicker.clearSelection();
     }
 
-    _updatePillItems() {
-        this.pillItems = this.selectedRecords.map(toContactPill);
-    }
-
-    _updateRecordPickerFilter() {
-        this.recordPickerFilter = toRecordPickerFilter(
-            this.selectedRecords.map((record) => record.id)
-        );
-    }
-
-    handleItemRemove(event) {
+    handlePillRemove(event) {
         const recordId = event.detail.item.name;
+
+        // Remove `recordId` from `selectedRecords`
         this.selectedRecords = this.selectedRecords.filter(
             (record) => record.id !== recordId
         );
-        this._updatePillItems();
-        this._updateRecordPickerFilter();
+    }
+
+    handleRecordPickerChange(event) {
+        this.recordId = event.detail.recordId;
+
+        // We want the record picker input to be cleared
+        // each time the user selects a record suggestion
+        this.refs.recordPicker.clearSelection();
     }
 }
