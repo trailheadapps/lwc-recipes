6fe23f86d8d1c709c82c1462338343387804efa5
Alba Rivas <aazcona@salesforce.com>
2024-01-17 10:51:22 +0100

    Add stylesheets recipe

---
+++ force-app/main/default/lwc/stylesheets/stylesheets.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import textStyles from './text-styles.css';
+
+export default class Stylesheets extends LightningElement {
+    static stylesheets = [textStyles];
+}
