944552bfa9d21de7f6ec9684cbfc36fad2b8fa87
Esteve Graells <esteve.graells@gmail.com>
2024-05-28 12:11:09 +0200

    additional own recipes: i18n and lwclogger
    Own recipes created to showcase i18n and how to send events from LWC to Event Monitoring

---
+++ force-app/main/default/lwc/egei18n/egei18n.js
@@ -0,0 +1,37 @@
+import { LightningElement } from "lwc";
+import USER_LOCALE from "@salesforce/i18n/locale";
+import USER_CURRENCY from "@salesforce/i18n/currency";
+
+
+export default class Egei18n extends LightningElement {
+    anotherLocale = "ja-JP"
+    get userLocale() {
+        return userLocale;
+    }
+
+    get today() {
+        const date = new Date();
+        return new Intl.DateTimeFormat(USER_LOCALE).format(date);
+    }
+
+    get todayOtherUser() {
+        const date = new Date();
+        return new Intl.DateTimeFormat(this.anotherLocale).format(date);
+    }
+
+    get hundredLocaleCurrency() {
+        return new Intl.NumberFormat(USER_LOCALE, {
+            style: "currency",
+            currency: USER_CURRENCY,
+            currencyDisplay: "symbol"
+        }).format(100);
+    }
+
+    get hundredOtherUserCurrency() {
+        return new Intl.NumberFormat(this.anotherLocale, {
+            style: "currency",
+            currency: USER_CURRENCY,
+            currencyDisplay: "symbol"
+        }).format(100);
+    }
+}
